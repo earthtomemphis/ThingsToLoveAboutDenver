c018b3c433c2aae50f44d0bacb58d28b279f2e99
Memphis DiAngelis <earthtomemphis@gmail.com>
2020-11-04 20:56:24 -0700

    updating posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import HomeView, ArticleDetailView, AddPostView
+from . views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('new-post/', AddPostView.as_view(), name='new-post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
 ]
