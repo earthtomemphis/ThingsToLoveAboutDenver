a503007af40b228b814eb2b22edccf9560c2a4cb
Memphis DiAngelis <earthtomemphis@gmail.com>
2020-11-05 04:01:04 -0700

    User Auth

--- ThingToLoveAboutDenverBlog/settings.py
+++ ThingToLoveAboutDenverBlog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'members',
     'crispy_forms',
 ]
 
@@ -120,5 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
