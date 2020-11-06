cbf3f901c454d71475e02355ceb0fb537f14c726
Memphis DiAngelis <earthtomemphis@gmail.com>
2020-11-05 20:48:04 -0700

    Added Like Button

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategorylistView
+from . views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategorylistView, LikeView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home"),
+    path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('new-post/', AddPostView.as_view(), name='new-post'),
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
     path('category/<str:cats>', CategoryView, name='category'),
     path('category-list/', CategorylistView, name='category-list'),
+    path('like/<int:pk>', LikeView, name='like_post'),
 ]
